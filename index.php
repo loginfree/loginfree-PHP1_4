14d395910baca08d7714c14cb536652f87f43013
loginfree <kara3dmax@gmail.com>
2019-01-11 13:03:04 +0300

    размещение

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<div class="content">
+	<a href="img/Freestyle.jpg" target="_blank"><img src="img/Freestyle.jpg" title="Freestyle YJS Carbon" width="200"></a>
+	<a href="img/Head%20Hj9.jpg"><img src="img/Head%20Hj9.jpg" alt="Head Hj9" title="Head Hj9" width="200"></a>
+	<a href="img/Seba.jpg"><img src="img/Seba.jpg" alt="Seba FR Deluxe 80" title="Seba FR Deluxe 80" width="200"></a>
+	<a href="img/Rollerblade.jpg"><img src="img/Rollerblade.jpg" alt="Rollerblade Twister Edge" title="Rollerblade Twister Edge" width="200"></a>
+</div>
+</body>
+</html>
